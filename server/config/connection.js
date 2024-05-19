f32ebb9eb6ae9bef0e76aaf895bf84c37a75daa2
Lex <info@alexandranel.com>
2024-05-19 13:50:14 +1000

    more trouble shooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    ssl: true,  // Ensure SSL is enabled
-    tlsAllowInvalidCertificates: true,  // Use this if you face certificate issues
+    ssl: true,
+    tlsAllowInvalidCertificates: true, // Use this only if you face certificate issues
 };
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/irl', options)
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit the process with an error code
+    });
 
 module.exports = mongoose.connection;
