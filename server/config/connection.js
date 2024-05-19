678040064ff2d9fe567c42ea418a8fa173e3d5f2
Lex <info@alexandranel.com>
2024-05-19 13:37:59 +1000

    updated MongoDB connection settings in an attempt to trouble shoot the SSL errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/irl');
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,  // Ensure SSL is enabled
+    tlsAllowInvalidCertificates: true,  // Use this if you face certificate issues
+};
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/irl', options)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
